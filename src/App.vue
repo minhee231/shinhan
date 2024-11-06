93b8124d2cac0c77e563b635f0becbc5f6863327
minhee231 <goominhee123@gmail.com>
2024-11-06 16:49:30 +0900

    feat: 아래 메뉴바 추가
    아직 색깔 수정도 안함

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <v-app>
     <v-main>
       <HelloWorld/>
+      <FooterMenu/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import FooterMenu from './components/FooterMenu.vue';
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    FooterMenu,
   },
 
   data: () => ({
